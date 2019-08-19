fafa0a82be3f3af237a9e271f7f91ba964d4110b
Chris Junker <elevatemedfit@gmail.com>
2019-08-19 13:47:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,6 +19,12 @@ def self.create_table
           DB[:conn].execute(sql)
 end
 
+def self.create(name:, grade:)
+  student = Student.new(name, grade)
+  student.save
+  student
+end
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
